7d3fa36307eaf76c00dc03b5c1ab6e38b1b0967e
FelixSuzdalev <felixsuzdalev@yandex.ru>
2024-02-13 21:59:04 +0300

    Cкибиди

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task_16
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите количество строк в массиве: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов в массиве: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random rand = new Random();
+            int[,] mn = new int[m, n];
+            int x = rand.Next(2, 5),k = rand.Next(2,5);
+            Console.WriteLine($"Рандомные значения: x = {x}, k = {k}\n");
+
+            Console.WriteLine($"Массив размерностью m = {m} на n = {n}\n");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    mn[i, j] = rand.Next(1, 10);
+                    Console.Write($"  {mn[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mn[i, j] == x)
+                    {
+                        mn[i, j] *= k;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nИзмененный массив:\n");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"  {mn[i, j].ToString().PadLeft(4)}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Task_6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_6_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //const int n = 8;
+            Console.WriteLine("Введите размерность квадратной матрицы:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    a[i, j] = j + 1 + i;
+                    Console.Write($" {a[i, j]} ");
+                }
+                for (int k = n - i; k < n; k++)
+                {
+                    a[i, k] = 0;
+                    Console.Write($" {a[i, k]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
